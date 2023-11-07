741bdd71acb0e66e6f8c99f08517eb4401fca655
Tonim06 <tonifp0610@gmail.com>
2023-11-07 19:08:27 +0100

    cambios5

--- js/personalWeb.js
+++ js/personalWeb.js
@@ -1,3 +1,16 @@
 window.addEventListener('load', function() {
     window.location.href = 'personalWeb.html#home';
 });
+
+
+const navLinks = document.querySelectorAll(".nav li a");
+navLinks.forEach(link => {
+    navLinks[0].classList.add("active");
+    link.addEventListener('click', () =>{
+        navLinks.forEach(link2 => {
+            link2.classList.remove("active");
+        });
+       link.classList.toggle("active"); 
+    })
+    
+});
